0943bd6738ca41fb760bcc2b8ec7f56a40422ee9
Marcelo Carranza <79749190+maoeneas@users.noreply.github.com>
2021-03-13 09:27:06 -0300

    Update ejercicios_profundizacion.py

--- ejercicios_profundizacion.py
+++ ejercicios_profundizacion.py
@@ -5,7 +5,6 @@ Ejercicios de profundización
 ---------------------------
 Autor: Inove Coding School
 Version: 1.3
-
 Descripcion:
 Programa creado para que practiquen los conocimietos
 adquiridos durante la semana
@@ -18,31 +17,31 @@ __version__ = "1.3"
 
 def ej1():
     # Ejercicios de práctica con números
-    # print('Nuestra primera calculadora')
-    # '''
-    # Realice un calculadora, se ingresará por línea de comando dos
-    # números reales y se deberá calcular todas las operaciones entre ellos:
-    # - Suma
-    # - Resta
-    # - Multiplicación
-    # - División
-    # - Exponente/Potencia
+    print('Nuestra primera calculadora')
+    '''
+    Realice un calculadora, se ingresará por línea de comando dos
+    números reales y se deberá calcular todas las operaciones entre ellos:
+    - Suma
+    - Resta
+    - Multiplicación
+    - División
+    - Exponente/Potencia
+    - Para todos los casos se debe imprimir en pantalla el resultado aclarando
+      la operación realizada en cada caso y con que números
+      se ha realizado la operación
+      ej: La suma entre 4.2 y 6.5 es 10.7
+    '''
+print ("modulo 2 ejercicio profundizacion 1")
+print ("ingrese un primer numero con 1 decimal")
+numero_1 = float(input())
+print("Ingrese segundo numero con 1 decimal")
+numero_2 = float(input())
+print("la suma de",numero_1, "y",numero_2 ,"es" ,numero_1+numero_2)
+print("la resta de",numero_1, "y",numero_2 ,"es" ,numero_1-numero_2)
+print("la multiplicacion de",numero_1, "y",numero_2 ,"es" ,numero_1*numero_2)
+print("la division de",numero_1, "y",numero_2 ,"es" ,numero_1/numero_2)
+print("la exponencializacion de ",numero_1, "y",numero_2 ,"es" ,numero_1**numero_2)    
 
-    # - Para todos los casos se debe imprimir en pantalla el resultado aclarando
-    #   la operación realizada en cada caso y con que números
-    #   se ha realizado la operación
-    #   ej: La suma entre 4.2 y 6.5 es 10.7
-    
-  print ("modulo 2 ejercicio profundizacion 1")
-  print ("ingrese un primer numero con 1 decimal")
-  numero_1 = float(input())
-  print("Ingrese segundo numero con 1 decimal")
-  numero_2 = float(input())
-  print("la suma de",numero_1, "y",numero_2 ,"es" ,numero_1+numero_2)
-  print("la resta de",numero_1, "y",numero_2 ,"es" ,numero_1-numero_2)
-  print("la multiplicacion de",numero_1, "y",numero_2 ,"es" ,numero_1*numero_2)
-  print("la division de",numero_1, "y",numero_2 ,"es" ,numero_1/numero_2)
-  print("la exponencializacion de ",numero_1, "y",numero_2 ,"es" ,numero_1**numero_2)
 def ej2():
     print('Ejercicios de práctica numérica y cadenas')
     '''
@@ -51,7 +50,6 @@ def ej2():
     - El DNI de la persona
     - La edad de la persona
     - La altura de la persona
-
     Finalmente el programa debe imprimir dos líneas de texto por separado
     - En una línea imprimir el nombre completo y el DNI, aclarando de que
       campo se trata cada uno
@@ -60,9 +58,18 @@ def ej2():
       altura de la persona
       Nuevamente debe aclarar el campo de cada uno, para el que lo lea
       entienda de que se está hablando.
-
     '''
-
+print ("modulo 2 ejercicio profundizacion 2")
+print("ingrese su nombre:")
+nombre = str(input())
+print("ingrese su DNI:")
+DNI = int(input())
+print("ingrese su altura:")
+altura = float(input())
+print("ingrese su edad:")
+edad= int(input())
+print("nombre completo:", nombre,"dni:",DNI)
+print("su edad es",edad,"su altura es:",altura)
 
 def ej3():
     print('Ejercicios de práctica con cadenas')
@@ -71,7 +78,6 @@ def ej3():
     Realice un programa que determine cual sería el apellido de una persona
     si se ingresaran los dos nombres completos de sus padres.
     Dicha persona deberá tener los apellidos de ambos padres
-
     - Primero el programa debe consultar el nombre completo del padre_1
     - Luego el programa debe consultar el nombre completo del padre_2
     - Luego debe consultar el nombre del hijo/a
@@ -80,7 +86,6 @@ def ej3():
       de sus padres
       y el nombre ingresado de dicha persona
     - Imprimir en pantalla el resultado
-
     NOTA: Para extraer el apellido del nombre completo recomendamos usar
     el método "split"
     Mostraremos un ejemplo:
@@ -96,6 +101,19 @@ def ej3():
     Cualquier duda con el método split pueden consultarla por el campus
 
     '''
+print ("Modulo 2 ejercicio profundizacion 3")
+print("ingrese el npmbre y apellido paterno: ")
+apepaterno = str(input()) 
+cortepaterno = apepaterno.split()
+finalapellido1=cortepaterno[-1]
+print(finalapellido1)
+print("ingrese el apellido nombre y apellido materno: ")
+apematerno = str(input())
+cortematerno = apematerno.split()
+finalapellido2=cortematerno[-1]
+print("ingrese su nombre de pila")
+nombrepila = str(input())
+print("su nombre completo es:",nombrepila,finalapellido1,finalapellido2)
 
 
 def ej4():
@@ -130,7 +148,19 @@ def ej4():
 
     Cualquier duda con el método split pueden consultarla por el campus
     '''
-
+print ("Modulo 2 ejercicio profundizacion 4")
+print("ingrese el npmbre y apellido de la primer persona: ")
+apeprimero = str(input()) 
+apellido1 = apeprimero.split()
+a=apellido1[-1]
+print("ingrese el apellido nombre y apellido de la segunda persona: ")
+apesegundo = str(input())
+apellido2 = apesegundo.split()
+b=apellido2[-1]
+if a == b:
+    print ( "es un familiar" )
+else:
+    print ( "NO es un familiar" )
 
 def ej5():
     # Ejercicios de práctica con cadenas
@@ -156,11 +186,17 @@ def ej5():
 
     Cualquier duda con estos métodos pueden consultarla por el campus
     '''
+print ("modulo 2 ejercicio profundizacion 5")
+print("ingrese su nombre completo:")
+nombre = str(input())
+print(nombre.lower())
+print(nombre.upper())
+print(nombre.capitalize())
 
 
 if __name__ == '__main__':
     print("Ejercicios de práctica")
-    ej1()
+    # ej1()
     # ej2()
     # ej3()
     # ej4()
